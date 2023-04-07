bbd7f947956730814cb87d6124b7c101ab1b9328
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-07 15:01:15 +0530

    Updated

---
+++ superchat/build/precache-manifest.33c685ce9d4be1c3d0562b3302b59b10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a748f16ccefe2e7f43f",
+    "url": "/static/css/main.b5cc555a.chunk.css"
+  },
+  {
+    "revision": "0a748f16ccefe2e7f43f",
+    "url": "/static/js/main.0a748f16.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "2c4593b122f936e3ee5f",
+    "url": "/static/js/2.2c4593b1.chunk.js"
+  },
+  {
+    "revision": "4ff977459512dd7b9c00",
+    "url": "/static/js/runtime~main.4ff97745.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f3bed30ec69e44b7b1f94eec9b8ad3a6",
+    "url": "/index.html"
+  }
+];
